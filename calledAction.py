11896cd14720376a1f6414a479bcd5caca33c0a9
TiredStudent <44681953+TiredStudent@users.noreply.github.com>
2018-11-12 13:35:15 -0500

    Create calledAction.py

---
+++ calledAction.py
@@ -0,0 +1,43 @@
+import subprocess
+from subprocess import call
+import ftplib
+from ftplib import FTP
+import sys
+from sys import modules
+import os
+import getpass
+what = input('What would you like to do? \nEncrypt a file (e), decrypt a file(d), or send a public key (s)? ')
+if what == "e":
+  filename = input('What file would you like to encrypt? ')
+  in_ad = input ('Where would you like to send this file? \nPlease input an IP address ')
+  ip_ad1 = ip_ad.replace(".", "!")
+  path = input('What was the path provided? ')
+  encryption = "publicKey" + ip_ad1
+  module = __import__(encryption).encrypt
+  curren = os.getcwd()
+  os.chdir(path)
+  f = open(filename, 'r').read()
+  os.chdir(current)
+  module(filename, f)
+  storing - "STOR" + filename
+  ftp = FTP(ip_ad)
+  f = open(filename, 'rb')
+  ftp.login(input('input your username '), getpass.getpass('input your password '))
+  ftp.storebinary(storing, f)
+  os.remove(filename)
+if what == "d":
+  filename = input('What file would you like to decrypt? ')
+  ip_ad = input 'Who wend this file? \nPlease input an IP address')
+  path = input ('What was the path provided? ')
+  ip_ad1 = ip_ad.replace(".", "!")
+  encryption = "privateKey" + ip_ad1
+  module = __import__(encryption).decrypt
+  current = os.getcwd()
+  os.chdir(path)
+  f = open(filename, 'rb').read()
+  os.chdir(current)
+  module(f,filename,path)
+if what =='s':
+  module = __import__('createKey')
+  module
+  
