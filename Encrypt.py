1d51186ce4a546bcdf85299ebb2b75014b343601
TiredStudent <44681953+TiredStudent@users.noreply.github.com>
2018-11-16 11:31:03 -0500

    Update Encrypt.py

--- Encrypt.py
+++ Encrypt.py
@@ -30,38 +30,53 @@ def int_to_file_decrypt (filename, integers, path):
 		f.write (byted)
 		i = i+1
 def file_to_array (f, n):
+	#this changes the file to an array of integers (each integer represents a character)
 	testing = n.to_bytes(7, byteorder = "big")
+	#the number used to separate the character is turned into bytes so that the computer can compare
 	bytelist = f.split(testing, maxsplit = -1)
+	#divides the list into an array in which each part is a byte for a different character, using the bytes for the testing
 	i = 0 
+	# intlist = [int.from_bytes(bytelist[i], byteorder = "big"), for i in bytelist
+	#check to replace below)
 	while i<len(bytelist):
-		p = int.from_bytelist[i], byteorder = "big")
+		p = int.from_bytes(bytelist[i], byteorder = "big")
+		#changes the character bytes into integers
 		bytelist[i] = p
 		i = i+1
 	del bytelist[len(bytelist)-1]
 	return bytelist
-def file_to_int(h):
-	i = 0
-	h = h.split()
-	integers = list()
-	while i<len(h):
-		p = int.from_bytes(h[i], byteorder = "big")
-		inegers.append(p)
-		i = i+1
-	return integers
+#pretty sure this isn't used below so probably delete file to int 
+#def file_to_int(h):
+#	i = 0
+#	h = h.split()
+#	integers = list()
+#	while i<len(h):
+#		p = int.from_bytes(h[i], byteorder = "big")
+#		inegers.append(p)
+#		i = i+1
+#	return integers
 def file_to_char(h):
+	#the file is called h (I will change it to a better name)
 	i = 0
 	characters = list()
 	while i < len(h):
+		#runs for the until the entire file has been put into the array
 		characters.append(h[i])
+		#puts each character in the file into the array
 		characters[i]= characters[i].encode()
+		#converts each part of the file into utf-8 integers
 		characters[i]= int.from_bytes(characters[i], byteorder = "big")
+		#converts each integer into bytes
 		i = i+1
 	return characters
 def gen_d (m, e):
+	#generates the d value for the equation 
 	hi = m
 	low = e
 	r = hi%low
+	#r for remainder from the high bound divided by low bound
 	x = (hi-r)/low
+	#gives the result of the division as an integer without the remainder
 	aMult = 0-x
 	mMult = 1
 	oriA = 1
@@ -87,6 +102,7 @@ def gen_e (m):
 	check = 2 
 	e = 1003242
 	while check <= (e**(1/2)):
+		#checks to ensure that e and m have no common factors
 		if e%check ==0 and m% check ==0:
 			e = e+1
 			check = 2
@@ -96,11 +112,14 @@ def gen_e (m):
 def gen_key():
 	up = 10000000
 	down = 100000
+	#defines the upper boundary and lowerboundary for the generation of large prime numbers
 	p = gen_prime(down, up)
 	q = gen_prime(down, up)
+	#two large prime numbers that are different from each other, but I need to add the comparison to each other
 	m, n= gen_m_n (p, q)
 	e = gen_e(m)
 	d = gen_d (m, e)
+	#n is the value shared between both private and public keys, but d is for the private key and e is for the public key
 	return n, e, d
 def gen_m_n(p, q):
 	m = (p-1)*(q-1)
@@ -110,6 +129,8 @@ def gen_prime (down, up):
 	p = random.randint(down, up)
 	check = 2
 	while check < (p**(1/2)):
+		#checks every possible factor up until the square root, at which it would begin repeating itself ensuring the creation of
+		#of a prime number without taking too much time checking factors
 		if p%check ==0 and p!=up:
 			p = p+1
 			check = 2
@@ -117,25 +138,33 @@ def gen_prime (down, up):
 			check = check +1
 	return p
 def mod_math (mess, exp, moded):
-	bitlist = list()
+	oldbitlist = list()
 	while exp!=0:
-		bitlist.append (exp%2)
+		oldbitlist.append (exp%2)
 		exp = (exp-(exp%2))/2
+		# give binary stuff like 1, 2, 4, 8, 16, 32 to break the number into more manageable pieces
+	#bitlist = [((oldbitlist(i)*2)**i) for i in oldbitlist]
+	#see if above works and delete below
 	i = 0
 	while i<len(bitlist):
-		bitlist [i] = bitlist[i]*2)**i
+		bitlist [i] = oldbitlist[i]*2)**i
 		i = i+1
 	i=0
 	newMess = 1
 	while i< len(bitlist):
+		#this is a method used so that the numbers aren't too big for the computer to do math with but remain accurate 
 		if bitlist[i] != 0:
 			temp = mess
 			counter = 0
 			while counter <i:
 				temp = temp**2
+				#takes it to the power of two before being moded and repeats because that is the same as
+				#the temp to the power of 2^i and being moded for each of the bits that make up the exponent needed
 				temp = temp%moded
 				counter = counter +1
 			bitlist[i] = temp
+			#probable can just put newMess = newMess*temp%moded
 			newMess = newMess*bitlist[i]%moded
+			#this works because a mod of a number is equal to it factors moded multiplied together and then moded again
 		i = i+1
 	return newMess
