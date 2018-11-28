0b5aee68a948543afbcd34e9b70f7f3d1d2d4970
TiredStudent <44681953+TiredStudent@users.noreply.github.com>
2018-11-28 18:40:58 -0500

    Add files via upload

--- Encrypt.py
+++ Encrypt.py
@@ -1,123 +1,146 @@
 import random
 from random import randint
 import os
-def int_to_file(filename, integers, known):
-	#writes over the file you're sending with the modified bytes and between each byte it has a default number both people have; n
-	looping=0
-	FILE = open(filename, 'w')
-	FILE = open(filename, 'a+b')
-	knownByte = known.to_bytes((known.bit_length()//8)+1, byteorder = "big")
-	# knownByte = known number in between each byte so that you can easily make a list for decrypting
-	for looping in integers:
-	#for num in integers:
-		FILE.write(looping.to_bytes((looping.bit_length()//8)+1, byteorder = "big"))
-		FILE.write(knownByte)
-		#changes each integer into a byte
-		#writes the bytes of the file
-		#goes through each character of the file
+def int_to_file(filename, integers, n):
+        i = 0
+        f = open(filename, 'w')
+        f = open(filename, 'a+b')
+        g = n.to_bytes(7, byteorder = "big")
+        while i < len(integers):
+            byted = integers[i].to_bytes(10, byteorder = "big")
+            f.write (byted)
+            f.write (g)
+            f.read()
+            i = i+1
 def int_to_file_decrypt (filename, integers, path):
-	os.chdir(path)
-	#opens the directory that holds the file needed
-	FILE = open(filename, 'w+')
-	FILE = open(filename, 'a+b')
-	for looping in integers
-	#for num in integers:
-		FILE = open(filename, 'a+b')
-		FILE.write(looping.to_bytes((looping.bit_length()//8)+1, byteorder = "big"))
-		#writes over the file with the decrypted characters
-def file_to_array (filename, known):
-	#this changes the file to an array of integers (each integer represents a character)
-	#the number used to separate the character is turned into bytes so that the computer can compare
-	bytelist = filename.split(known.to_bytes((known.bit_length()//8)+1), maxsplit = -1)
-	#divides the list into an array in which each part is a byte for a different character, using the bytes for the testing
-	intlist = [int.from_bytes(byteNum, byteorder = "big") for byteNum in bytelist]
-	del inlist[len(intlist)-1]
-	return intlist
-def file_to_char(filename):
-	characters = [(int.from_bytes(parts.encode(),byteorder = "big")) for parts in filename]
-		#puts each character in the file into the array
-		#converts each part of the file into utf-8 integers
-		#converts each integer into bytes
-	return characters
+        os.chdir(path)
+        f = open(filename, 'w+')
+        f = open(filename, 'a+b')
+        i = 0
+        while i < len(integers):
+            byted= integers[i].to_bytes(10, byteorder = "big")
+            f = open(filename, 'a+b')
+            f.write (byted)
+            i = i+1
+def file_to_array (f, n):
+    testing = n.to_bytes(7, byteorder = "big")
+    #f = open(filename, 'rb')
+    #f = f.read()
+    bytelist = f.split (testing, maxsplit = -1)
+    i = 0
+    while i< len(bytelist):
+        p = int.from_bytes(bytelist[i], byteorder = "big")
+        bytelist[i] = p
+        i = i+1
+    del bytelist[len(bytelist)-1]
+    return bytelist
+def file_to_int(h):
+    i = 0
+    h = h.split()
+    #h = open(filename, 'r+b').read()
+    integers = list()
+    while i<len(h):
+        p = int.from_bytes(h[i], byteorder = "big")
+        integers.append(p)
+        i = i+1
+    return integers
+def file_to_char (h):
+    i = 0
+    characters = list()
+    while i < len(h):
+        characters.append(h[i])
+        characters[i]= characters[i].encode()
+        characters[i] = int.from_bytes(characters[i], byteorder = "big")
+        #characters[i] = int(characters[i])
+        i = i+1
+    return characters
 def gen_d (m, e):
-	#generates the d value for the equation 
-	hi = m, low = e
-	r = hi%low
-	#r for remainder from the high bound divided by low bound
-	x = (hi-r)/low
-	#gives the result of the division as an integer without the remainder
-	aMult = 0-x, mMult = 1, oriA = 1, oriM = 0, hi = low
-	low = r
-	while low!= 1:
-		r = hi%low
-		x = (hi-r)/low
-		tempM = mMult, tempA = aMult
-		mMult = oriM - (x*mMult)
-		aMult = oriA - (x*aMult)
-		oriA = tempA, oriM = tempM
-		hi = low
-		low = r
-	d = aMult
-	if aMult <0:
-		d = m+aMult
-	return d
+    hi = m
+    low = e
+    r = hi% low
+    x = (hi-r)/low
+    aMult = 0-x
+    mMult = 1
+    oriA = 1
+    oriM= 0
+    hi = low
+    low = r
+    while low!= 1:
+        r = hi%low
+        x = (hi-r)/low
+        tempM = mMult
+        tempA = aMult
+        mMult = oriM -(x*mMult)
+        aMult = oriA - (x*aMult)
+        oriA = tempA
+        oriM = tempM
+        hi = low
+        low = r
+    d = aMult
+    if aMult <0:
+        d = m+aMult
+    return d
 def gen_e (m):
-	check = 2 
-	e = 1003242
-	while check <= (e**(1/2)):
-		#checks to ensure that e and m have no common factors 
-		#it is raised to the power of 1/2 because if it doesn't have any common factors until its square root, it won't have any
-		if e%check ==0 and m% check ==0:
-			e +=1
-			check = 2
-		else:
-			check +=1
-	return e
+    check = 2
+    e = 1003242
+    while check <= (e**(1/2)):
+        if e%check == 0 and m% check ==0:
+            e = e+1
+            check =2
+        else:
+                check = check +1
+    return e
 def gen_key():
-	up = 10000000
-	down = 100000
-	#defines the upper boundary and lowerboundary for the generation of large prime numbers
-	primeNum1 = gen_prime(down, up)
-	primeNum2 = gen_prime(down, up)
-	#two large prime numbers that are different from each other, but I need to add the comparison to each other
-	m, n= gen_m_n (primeNum1, primeNum2)
-	e = gen_e(m)
-	d = gen_d (m, e)
-	#n is the value shared between both private and public keys, but d is for the private key and e is for the public key
-	return n, e, d
-def gen_m_n(num1, num2):
-	m = (num1-1)*(num2-1)
-	n = num1*num2
-	return m, n
+    up = 10000000
+    down = 100000
+    p = gen_prime(down, up)
+    q = gen_prime(down, up)
+    m, n = gen_m_n (p, q)
+    e =gen_e (m)
+    d = gen_d (m, e)
+    return n, e, d
+def gen_m_n(p, q):
+    m = (p-1)*(q-1)
+    n = p*q
+    return m, n
 def gen_prime (down, up):
-	prime = random.randint(down, up)
-	check = 2
-	while check < (prime**(1/2)):
-		#checks every possible factor up until the square root, at which it would begin repeating itself ensuring the creation of
-		#of a prime number without taking too much time checking factors
-		if prime%check ==0 and prime!=up:
-			prime +=1
-			check = 2
-		else:
-			check +=1
-	return prime
+    p = random.randint(down, up)
+    check = 2
+    while check < (p**(1/2)):
+        if p%check ==0 and p!= up:
+            p = p+1
+            check = 2
+        elif p == up:
+            p = random.randint(down,up)
+            check = 2
+        else:
+            check = check +1
+    return p
 def mod_math (mess, exp, moded):
-	oldbitlist = list()
-	while exp!=0:
-		oldbitlist.append (exp%2)
-		exp = (exp-(exp%2))/2
-		# give binary stuff like 1, 2, 4, 8, 16, 32 to break the number into more manageable pieces
-	bitlist = [((oldBits*2)**i) for oldBits in oldbitlist]
-	newMess = 1
-	for looping in range(len(bitlist)):
-		#this is a method used so that the numbers aren't too big for the computer to do math with but remain accurate 
-		if bitlist[looping] != 0:
-		      for counter in range(looping);
-				temp = (mess**2)%moded
-				#takes it to the power of two before being moded and repeats because that is the same as
-				#the temp to the power of 2^i and being moded for each of the bits that make up the exponent needed
-			bitlist[looping] = temp
-			#probable can just put newMess = newMess*temp%moded
-			newMess = newMess*bitlist[looping]%moded
-			#this works because a mod of a number is equal to it factors moded multiplied together and then moded again
-	return newMess
+    bitlist = list()
+    while exp!=0:
+        bitlist.append (exp%2)
+        exp = (exp-(exp%2))/2
+    i = 0
+    while i<len(bitlist):
+        bitlist [i] = (bitlist[i]*2)**i
+        i = i+1
+    i = 0
+    newMess = 1
+    while i < len(bitlist):
+        if bitlist [i] != 0:
+            temp = mess
+            counter = 0
+            while counter < i:
+                temp = temp**2
+                temp = temp%moded
+                counter = counter +1
+            bitlist [i] = temp
+            newMess = newMess*bitlist[i]%moded
+        i = i+1
+    #while i < len(bitlist):
+     #   if bitlist[i]!=0:
+    #        newMess = newMess * bitlist[i]
+    #    i = i+1
+   # newMess = newMess %moded
+    return newMess
--- calledAction.py
+++ calledAction.py
@@ -6,46 +6,38 @@ import sys
 from sys import modules
 import os
 import getpass
-what = input('What would you like to do? \nEncrypt a file (e), decrypt a file(d), or send a public key (s)? ')
+what = input('What would you like to do? \nEncrypt a file (e), decrypt a file (d), or send a public key (s)? ')
 if what == "e":
-  #calls for encrypting a file
-  filename = input('What file would you like to encrypt? ')
-  #requires the person to start in the directory in which the file is stored to get the path for the filename
-  in_ad = input ('Where would you like to send this file? \nPlease input an IP address ')
-  ip_ad1 = ip_ad.replace(".", "!")
-  path = input('What was the path provided? ')
-  #the path provided was the shown due to the alias created in the system for people who are inept at dealing with computers
-  encryption = "publicKey" + ip_ad1
-  module = __import__(encryption).encrypt
-  #this loads the file with the name saved through the ip address and the "def" of encryption
-  current = os.getcwd()
-  #so that the computer will be able to change directories to and from the file place and the encryption place
-  os.chdir(path)
-  f = open(filename, 'r').read()
-  #reads the file while it is in that directory so it can go back the the directory with the code and finish doing stuff with it
-  os.chdir(current)
-  module(filename, f)
-  #runs the encryption with the file
-  ftp = FTP(ip_ad)
-  f = open(filename, 'rb')
-  ftp.login(input('input your username '), getpass.getpass('input your password '))
-  ftp.storebinary('STOR' + filename, f)
-  os.remove(filename)
-  #removes the new file that was just created on the computer as it was saved to the ftp file in the other computer
+    filename = input('What file would you like to encrypt? ')
+    ip_ad = input('Where would you like to send this file? \nPlease input an IP address ')
+    ip_ad1 = ip_ad.replace(".", "!")
+    path = input ('What was the path provided? ')
+    encryption = "publicKey" + ip_ad1
+    module = __import__(encryption).encrypt
+    current = os.getcwd()
+    os.chdir(path)
+    f = open(filename, 'r').read()
+    os.chdir(current)
+    module(filename, f)
+    storing = "STOR "+ filename
+    ftp = FTP(ip_ad)
+    f = open(filename, 'rb')
+    ftp.login(input('input your username '), getpass.getpass('input your password '))
+    ftp.storbinary(storing, f)
+    os.remove(filename)
 if what == "d":
-  filename = input('What file would you like to decrypt? ')
-  ip_ad = input ('Who sent this file? \nPlease input an IP address')
-  #allows the computer to grab the correct key that corresponds to the other person
-  path = input ('What was the path provided? ')
-  ip_ad1 = ip_ad.replace(".", "!")
-  encryption = "privateKey" + ip_ad1
-  module = __import__(encryption).decrypt
-  current = os.getcwd()
-  os.chdir(path)
-  f = open(filename, 'rb').read()
-  os.chdir(current)
-  module(f,filename,path)
+    filename = input('What file would you like to decrypt? ')
+    ip_ad = input('Who sent this file? \nPlease input an IP address ')
+    path = input ('What was the path provided? ')
+    ip_ad1 = ip_ad.replace(".", "!")
+    encryption = "privateKey" + ip_ad1
+    module = __import__(encryption).decrypt
+    current = os.getcwd()
+    os.chdir(path)
+    f = open(filename, 'rb').read()
+    os.chdir(current)
+    module(f, filename, path)
 if what =='s':
-  module = __import__('createKey')
-  module
-  
+    module = __import__('createKey')
+    module
+    
